b23e28bd27211c6a16621364a249c58fb0a42568
Jeremiah Coyle <jcoyle@gmail.com>
2019-06-12 20:24:38 -0400

    Add isInsideFn

--- src/form.js
+++ src/form.js
@@ -1,92 +1,98 @@
 const util = require('./util');
+const vscode = require('vscode');
 
-function reTestOpeningBracket (char){
+function reTestOpeningBracket(char) {
   return /\(|\[|\{/.test(char);
 }
 
-function reTestClosingBracket (char){
+function reTestClosingBracket(char) {
   return /\)|\]|\}/.test(char);
 }
 
-function charAtIndex(idx, buffText){
-  return buffText.substring(idx, idx+1);
+function charAtIndex(idx, buffText) {
+  return buffText.substring(idx, idx + 1);
 }
 
-function findClosingBraceIdx(startIdx, state){
-  let closingChars = {"(":")", "{":"}", "[":"]"};
+function findClosingBraceIdx(startIdx, state) {
+  let closingChars = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
+  };
   let allBraces = ["(", ")", "{", "}", "[", "]"];
   let openingBraceChar = util.charAtIdx(state, startIdx);
   let stack = [openingBraceChar];
   let cur, i, targetOpeningBrace;
-  for (i = startIdx+1; i < state.endOffset; i++ ){
+  for (i = startIdx + 1; i < state.endOffset; i++) {
     cur = util.charAtIdx(state, i);
-    if(!state.blackListedPos[i]){
-      targetOpeningBrace = stack[stack.length-1];
-      if(cur === closingChars[targetOpeningBrace]){
+    if (!state.blackListedPos[i]) {
+      targetOpeningBrace = stack[stack.length - 1];
+      if (cur === closingChars[targetOpeningBrace]) {
         stack.pop();
-      }else if((allBraces.indexOf(cur) > -1)){
+      } else if ((allBraces.indexOf(cur) > -1)) {
         stack.push(cur);
       }
     }
-    if(stack.length === 0 ){
+    if (stack.length === 0) {
       break;
     }
   }
   return i;
 }
 
-function indexForms(i, state){
+function indexForms(i, state) {
   let char = charAtIndex(i, state.buffText);
   let skip = state.blackListedPos[i];
-  if(!skip){
-    if(reTestOpeningBracket(char)){
+  if (!skip) {
+    if (reTestOpeningBracket(char)) {
       state.curDepthIdx.push(i);
     }
   }
-  state.allPos[i] = state.curDepthIdx.map(function(v){return v});
-  if(!skip){
-    if(reTestClosingBracket(char)){
+  state.allPos[i] = state.curDepthIdx.map(function (v) {
+    return v
+  });
+  if (!skip) {
+    if (reTestClosingBracket(char)) {
       state.curDepthIdx.pop();
     }
   }
 }
 
-function idxIsInForm(state, idx){
+function idxIsInForm(state, idx) {
   return state.allPos[idx].length;
 }
 
-function stepFwdCondition(state, counter, formOffset){
+function stepFwdCondition(state, counter, formOffset) {
   let formStartsArray = state.allPos[counter];
-  if(Array.isArray(formStartsArray)){
+  if (Array.isArray(formStartsArray)) {
     return formStartsArray[0] === formOffset;
-  }else{
+  } else {
     return false;
   }
 }
 
-function rangeOuterForm(state){
+function rangeOuterForm(state) {
   let point = state.isPointFollowingForm || state.isPointFollowingExpression ?
-    state.ogPointIdx - 1
-    :
+    state.ogPointIdx - 1 :
     state.ogPointIdx;
   let formStartsArray = state.allPos[point];
   let depth = formStartsArray.length;
-  if(!depth){
+  if (!depth) {
     return;
   }
 
-  if(depth === 1 && state.rangeCurrentForm){
+  if (depth === 1 && state.rangeCurrentForm) {
     return state.rangeCurrentForm;
   }
 
   let formOffset = formStartsArray[0];
   let counter = formStartsArray[0];
 
-  const stepFwd = function stepFwd(){
+  const stepFwd = function stepFwd() {
     counter++;
   }
-  while(stepFwdCondition(state, counter, formOffset)){
-   stepFwd();
+  while (stepFwdCondition(state, counter, formOffset)) {
+    stepFwd();
   }
 
   return {
@@ -97,7 +103,7 @@ function rangeOuterForm(state){
 
 function endOfFormCondition(state, counter, depth) {
   let formStartsArray = state.allPos[counter];
-  if(!Array.isArray(formStartsArray)){
+  if (!Array.isArray(formStartsArray)) {
     return false;
   }
   let bools;
@@ -106,7 +112,7 @@ function endOfFormCondition(state, counter, depth) {
   } catch (error) {
     //console.log("endOfFormCondition", error);
   }
-  
+
   return bools;
 }
 
@@ -116,14 +122,14 @@ function rangeCurrentForm(state, checkPreceding) {
   let formStartsArray = state.allPos[point];
 
   let depth = formStartsArray.length;
-  if(!depth){ 
+  if (!depth) {
     return;
   }
 
   let formOffset = formStartsArray[depth - 1];
   let counter = point;
-  
-  while (endOfFormCondition(state, counter, depth)){
+
+  while (endOfFormCondition(state, counter, depth)) {
     counter++;
   }
   return {
@@ -133,57 +139,65 @@ function rangeCurrentForm(state, checkPreceding) {
 }
 
 
-function blackListRange (range, state, blackListedRangeIdx) {
+function blackListRange(range, state, blackListedRangeIdx) {
   let rangeArr = util.numRange(range.start, range.end, true);
-  rangeArr.forEach(function(idx){
+  rangeArr.forEach(function (idx) {
     state.blackListedPos[idx] = blackListedRangeIdx;
   });
 }
 
-function isRangeStartBlackListed (range, state){
+function isRangeStartBlackListed(range, state) {
   return state.blackListedPos[range.start];
 }
 
 
-function blackListRangeByType(state, range, type, rangeFn){
-  if(!isRangeStartBlackListed(range, state)){
+function blackListRangeByType(state, range, type, rangeFn) {
+  if (!isRangeStartBlackListed(range, state)) {
     let idxRange = rangeFn ? rangeFn(range, state) : range;
     let pointRange = util.toPointRange(state, idxRange);
     let updatedIdxRange = util.toIdxRange(state, pointRange);
-    state.blackListedRangesByIdx.push({type: type, range: pointRange});
+    state.blackListedRangesByIdx.push({
+      type: type,
+      range: pointRange
+    });
 
-    blackListRange(updatedIdxRange, state, state.blackListedRangesByIdx.length-1);
+    blackListRange(updatedIdxRange, state, state.blackListedRangesByIdx.length - 1);
   }
 }
 
-function findIgnoredFormOrMetaMapRange(range, state){
-    const openingBraceIdx = range.end-1;
-    const closingBraceIdx = findClosingBraceIdx(openingBraceIdx, state);
-    const ignoredFormRange = {start: range.start, end: closingBraceIdx+1};
-    return ignoredFormRange;
+function findIgnoredFormOrMetaMapRange(range, state) {
+  const openingBraceIdx = range.end - 1;
+  const closingBraceIdx = findClosingBraceIdx(openingBraceIdx, state);
+  const ignoredFormRange = {
+    start: range.start,
+    end: closingBraceIdx + 1
+  };
+  return ignoredFormRange;
 }
 
-function addCharsForHashSet(text, formOffset, state){
-  if(/^\{/.test(text)){
-    if(util.charAtIdx(state, formOffset-1) === "#"){
-      return "#"+text;
+function addCharsForHashSet(text, formOffset, state) {
+  if (/^\{/.test(text)) {
+    if (util.charAtIdx(state, formOffset - 1) === "#") {
+      return "#" + text;
     }
   }
 }
 
-function addCharsForReaderConds(text, formOffset, state){
-  if(/^\(/.test(text)){
-    let hasPound = util.charAtIdx(state, formOffset-2) === "#";
-    let hasQuestionMark = util.charAtIdx(state, formOffset-1) === "?";
-    if(hasPound && hasQuestionMark){
+function addCharsForReaderConds(text, formOffset, state) {
+  if (/^\(/.test(text)) {
+    let hasPound = util.charAtIdx(state, formOffset - 2) === "#";
+    let hasQuestionMark = util.charAtIdx(state, formOffset - 1) === "?";
+    if (hasPound && hasQuestionMark) {
       return "#?" + text;
     }
   }
 }
 
-function prependSpecialChars (state, rangeProp){
+function prependSpecialChars(state, rangeProp) {
   let range = state[rangeProp];
-  if (!range){ return null; }
+  if (!range) {
+    return null;
+  }
 
   let text = util.getTextInPointRange(state, range);
   let formOffset = util.idxForPos(state, range.start);
@@ -191,20 +205,23 @@ function prependSpecialChars (state, rangeProp){
 }
 
 
-function setJsComment(state){
+function setJsComment(state) {
 
   let textInRange = util.getTextInPointRange(state, state.rangeOuterForm);
   let m = /^\(comment\s+\:js\s+([\s\S]+)/gm.exec(textInRange);
-  if(m){
+  if (m) {
     let startOffset = textInRange.indexOf(m[1]);
     let startIdx = util.idxForPos(state, state.rangeCurrentForm.start) + startOffset;
     let endIdx = util.idxForPos(state, state.rangeCurrentForm.end);
-    state.jsCommentEvalRange = util.toPointRange(state, {start: startIdx, end: endIdx});
-    state.jsComment = m[1].slice(0,-1);
+    state.jsCommentEvalRange = util.toPointRange(state, {
+      start: startIdx,
+      end: endIdx
+    });
+    state.jsComment = m[1].slice(0, -1);
     state.jsCommentSelectedText = state.selectedText;
     state.isJsComment = true;
     state.isNotJsComment = false;
-  }else{
+  } else {
     state.jsComment = null;
     state.isJsComment = false;
     state.isNotJsComment = true;
@@ -212,21 +229,24 @@ function setJsComment(state){
   return m;
 }
 
-function bufferScan(state, type, rangeFn){
+function bufferScan(state, type, rangeFn) {
   const reByType = {
-   "string": /\#"([^"]|\\")*"|"([^\\"]|\\\\|\\")*"/gm,
-   "comment": /;.*/g,
-   "ignoredForm": /\#_\(|\#_\[|\#_\{/gm,
-   "metaMap": /\#\^{/g
+    "string": /\#"([^"]|\\")*"|"([^\\"]|\\\\|\\")*"/gm,
+    "comment": /;.*/g,
+    "ignoredForm": /\#_\(|\#_\[|\#_\{/gm,
+    "metaMap": /\#\^{/g
   }
   const re = reByType[type];
   let m;
   while (m = re.exec(state.buffText)) {
-    blackListRangeByType(state, {start:m.index, end:re.lastIndex}, type, rangeFn);
+    blackListRangeByType(state, {
+      start: m.index,
+      end: re.lastIndex
+    }, type, rangeFn);
   }
 }
 
-function setupBlackListProps(state){
+function setupBlackListProps(state) {
   state.blackListedOffsets = Object.create(null);
   state.blackListedPos = new Array();
   state.blackListedPos.length = state.endOffset;
@@ -236,7 +256,7 @@ function setupBlackListProps(state){
   state.curDepthIdx = new Array();
 }
 
-function blackListAndIndexForms (state){
+function blackListAndIndexForms(state) {
   setupBlackListProps(state);
 
   // blacklist regex literal ranges or string ranges
@@ -253,7 +273,7 @@ function blackListAndIndexForms (state){
 
   // index forms
   let i = 0;
-  for(i; i < state.endOffset; i++){
+  for (i; i < state.endOffset; i++) {
     indexForms(i, state);
   }
 
@@ -261,7 +281,7 @@ function blackListAndIndexForms (state){
   state.blackListedRangeIdx = state.blackListedPos[state.ogPointIdx];
   state.blackListedRange = state.blackListedRangesByIdx[state.blackListedRangeIdx];
 
-  if(state.blackListedRange){
+  if (state.blackListedRange) {
     state.isBlacklisted = true;
     state.isCommentRange = state.blackListedRange.type === "comment";
     state.isIgnoredFormRange = state.blackListedRange.type === "ignoredForm";
@@ -270,19 +290,19 @@ function blackListAndIndexForms (state){
 }
 
 
-function charExistsAtIdx(state, idx){
-  let char = state.buffText.substring(idx, idx+1);
+function charExistsAtIdx(state, idx) {
+  let char = state.buffText.substring(idx, idx + 1);
   let isValid = /[^\s\n]/.test(char);
   return isValid;
 }
 
 
 // Evaluate an expression inside form (var, symbol, keyword, number, etc.)
-function evalExpressionInsideForm(state){
+function evalExpressionInsideForm(state) {
   let point = state.isPointFollowingExpression ? state.ogPointIdx - 1 : state.ogPointIdx;
   let formContents = state.textCurrentForm.substr(1).slice(0, -1);
   let rangeStartIndex = util.idxForPos(state, state.rangeCurrentForm.start);
-  let rangeContentsStartIndex = rangeStartIndex+1;
+  let rangeContentsStartIndex = rangeStartIndex + 1;
   let re = /\#"([^"]|\\")*"|"([^\\"]|\\\\|\\")*"|([^\s\"]+)/gm;
   let expObj = util.expressionAtIdx(
     formContents,
@@ -291,19 +311,22 @@ function evalExpressionInsideForm(state){
     rangeContentsStartIndex
   );
 
-  if(expObj){
+  if (expObj) {
     let start = util.posForIdx(state, expObj.startIdx);
     let end = util.posForIdx(state, expObj.endIdx);
     state.isPointOnExpression = true;
-    state.rangeCurrentExpression = {start: start, end: end};
+    state.rangeCurrentExpression = {
+      start: start,
+      end: end
+    };
     state.textCurrentExpression = expObj.matchedString;
   }
 }
 
 // Evaluate an expression outside form (var, symbol, keyword, number, etc.)
-function evalExpressionOutsideForm(state){
+function evalExpressionOutsideForm(state) {
   let point = state.isPointFollowingExpression ? state.ogPointIdx - 1 : state.ogPointIdx;
-  if(state.isStringRange){
+  if (state.isStringRange) {
     state.rangeCurrentExpression = state.blackListedRange.range;
     state.textCurrentExpression = util.getTextInPointRange(state, state.rangeCurrentExpression);
     return;
@@ -311,63 +334,63 @@ function evalExpressionOutsideForm(state){
   let idxEnd = point;
   let idxStart = point;
 
-  if(charExistsAtIdx(state, point)){
+  if (charExistsAtIdx(state, point)) {
     idxEnd++;
     idxStart--;
-    while(!idxIsInForm(state, idxEnd) && charExistsAtIdx(state, idxEnd)){
+    while (!idxIsInForm(state, idxEnd) && charExistsAtIdx(state, idxEnd)) {
       idxEnd++;
     }
-    while(!idxIsInForm(state, idxStart) && charExistsAtIdx(state, idxStart)){
+    while (!idxIsInForm(state, idxStart) && charExistsAtIdx(state, idxStart)) {
       idxStart--;
     }
     idxStart++;
     state.isPointOnExpression = true;
-    state.rangeCurrentExpression = util.toPointRange(state, {start: idxStart, end: idxEnd});
+    state.rangeCurrentExpression = util.toPointRange(state, {
+      start: idxStart,
+      end: idxEnd
+    });
     state.textCurrentExpression = util.getTextInPointRange(state, state.rangeCurrentExpression);
   }
 }
 
-
-
-
-
 // Evaluate an expression inside a form (string, var, symbol, keyword, number, etc.)
-function addExpressionRange(state){
-  if(state.isJs){return;}
-  if(state.isJsComment || state.isCommentRange || state.isIgnoredFormRange){
+function addExpressionRange(state) {
+  if (state.isJs) {
+    return;
+  }
+  if (state.isJsComment || state.isCommentRange || state.isIgnoredFormRange) {
     state.isPointNotOnExpression = false;
     state.rangeCurrentExpression = null;
     return;
   }
 
-  if(state.isInsideForm){
+  if (state.isInsideForm) {
     evalExpressionInsideForm(state);
   }
 
-  if(state.isOutsideForm){
+  if (state.isOutsideForm) {
     evalExpressionOutsideForm(state);
   }
 }
 
-function outsideForm(state){
-  if(state.blackListedRangeIdx){
-    if(state.blackListedRange.type === "string"){
+function outsideForm(state) {
+  if (state.blackListedRangeIdx) {
+    if (state.blackListedRange.type === "string") {
       state.rangeCurrentExpression = state.blackListedRange.range;
       state.textCurrentExpression = util.getTextInPointRange(state, state.rangeCurrentExpression);
     }
-  }else{
+  } else {
     state.isNotBlacklisted = true;
   }
 }
 
-function insideForm(state){
+function insideForm(state) {
   state.rangeOuterForm = rangeOuterForm(state)
-  
+  state.isInsideFn = isInsideFn(state);
   setJsComment(state);
   state.textCurrentForm = prependSpecialChars(state, "rangeCurrentForm");
   state.textOuterForm = (state.rangeCurrentForm === state.rangeOuterForm) ?
-    state.textCurrentForm
-    :
+    state.textCurrentForm :
     prependSpecialChars(state, "rangeOuterForm");
   state.textOuterForm = util.getTextInPointRange(state, state.rangeOuterForm);
 }
@@ -412,9 +435,19 @@ function isPointFollowingForm(state) {
   return pcIsClosing;
 }
 
+function isInsideFn(state) {
+  const start = state.rangeOuterForm.start;
+  const line = start.line;
+  const char = start.character;
+  const range = new vscode.Range(line, char, line, char + 5);
+  const text = util.getTextInPointRange(state, range);
+  return text === `(defn`
+}
 
-function addFormRange(state){
-  if(state.isJs){return;}
+function addFormRange(state) {
+  if (state.isJs) {
+    return;
+  }
 
   // add blacklisting info to state
   blackListAndIndexForms(state);
@@ -426,9 +459,9 @@ function addFormRange(state){
   state.isInsideForm = state.rangeCurrentForm ? true : false;
   state.isOutsideForm = (!state.rangeCurrentForm);
 
-  if(state.isInsideForm){
+  if (state.isInsideForm) {
     insideForm(state);
-  }else{
+  } else {
     outsideForm(state);
   }
 }
